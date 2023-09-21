5631b78ce299bfd1a5cd28e3c47e9e5ad616009c
MIDRING <133687460+midring-co@users.noreply.github.com>
2023-09-22 00:52:38 +0200

    1.0

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,48 +1,7 @@
-import React, { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
-
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { useDispatch, useSelector } from 'react-redux';
-import { logout } from './redux/reducers';
-
-import Loader from './components/loader';
-
-const theme = createTheme();
+import React from 'react';
 
 const App = () => {
-  const reduxToken = useSelector(state => state.token);
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
 
-  const redirectClient = () => {
-    if(reduxToken) {
-      axios.post('http://localhost:8000/api/login/check-token', reduxToken, { timeout: 10000 })
-      .then(response => {
-        if(response.data.message === 'VALID_LOGIN_BY_TOKEN') {
-          navigate('/dashboard');
-        } else {
-          dispatch(logout({}));
-          navigate('/login');
-        }
-      }).catch(error => {
-        dispatch(logout({}));
-        navigate('/login');
-      });
-    } else {
-      navigate('/login');
-    }
-  };
-
-  useEffect(() => {
-    redirectClient();
-  }, []);
-
-  return (
-   <ThemeProvider theme={theme}>
-    <Loader/>
-   </ThemeProvider>
-  );
 };
 
 export default App;
--- front-end/src/index.js
+++ front-end/src/index.js
@@ -2,30 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import App from './App';
-import NotFound from './NotFound';
-import Login from './pages/auth/Login';
-import ResetPassword from './pages/auth/ResetPassword';
-import ResetPasswordRequest from './pages/auth/ResetPasswordRequest';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import { store, persistor } from './redux/store';
-import Loader from './components/loader';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={<Loader/>} persistor={persistor}>
-        <Router>
-          <Routes>
-            <Route exact path='/' element={<App/>}></Route>
-            <Route exact path='/login' element={<Login/>}></Route>
-            <Route exact path='/reset-password' element={<ResetPassword/>}></Route>
-            <Route exact path='/reset-password-request/:login/:securitycode/:token' element={<ResetPasswordRequest/>}></Route>
-            <Route path='*' element={<NotFound/>}></Route>
-          </Routes>
-        </Router>
-      </PersistGate>
-    </Provider>
+    <Router>
+      <Routes>
+        <Route exact path='/' element={<App/>}></Route>
+        <Route path='*' element={<NotFound/>}></Route>
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
